a50aeeab12162c77d7502f5e525cc5662ba37a89
Aleksei Starikov <starikovaleksei1992@mail.ru>
2018-06-16 19:25:19 +0300

    create dirs and files

--- src/main/java/ru/star/Main.java
+++ src/main/java/ru/star/Main.java
@@ -1,7 +1,34 @@
 package ru.star;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        new WikiPrinter().print("Политика");
+    public static void main(String[] args) throws IOException {
+//        String[] ar = new String[]{"Искусство", "Автомобили", "Бизнес", "Путешествия", "Религия", "Политика", "Наука", "Домашние животные", "Спорт", "Технология"};
+//        Arrays.sort(ar);
+//        System.out.println(Arrays.toString(ar));
+//        List<String> lines = Arrays.asList("The first line", "The second line");
+////        Path file = Paths.get("/directory/the-file-name.txt");
+//        File tmp = new File("logs/error.log");
+//        tmp.getParentFile().mkdirs();
+//        tmp.createNewFile();
+//
+//        File file = new File("directory/the-file-name.txt");
+//        if(!file.exists()) {
+//            System.out.println(file.canWrite());
+//            System.out.println(file.getParentFile().mkdirs());
+//        }
+//        Files.write(Paths.get(file.getPath()), lines, Charset.forName("UTF-8"));
+
+        new WikiPrinter().print("Политика", 1, "");
+        new WikiPrinter().print("Искусство", 1, "");
+        new WikiPrinter().print("Автомобили", 1, "");
     }
 }
--- src/main/java/ru/star/WikiPrinter.java
+++ src/main/java/ru/star/WikiPrinter.java
@@ -5,6 +5,8 @@ import ru.star.model.Article;
 import ru.star.model.Category;
 import ru.star.parser.json.Parser;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import static ru.star.parser.json.Parser.parseCategories;
@@ -18,7 +20,10 @@ public class WikiPrinter {
         articleCounter = 0;
     }
 
-    public void print(String category) {
+    public void print(String category, int id, String preventDirs) throws IOException {
+        String fileName = preventDirs + (preventDirs.equals("") ? "" : File.separator) + "_" + category;
+        File file = new File(fileName);
+        file.mkdir();
         System.out.println("Печатаю категорию - " + category + "; articleCounter = " + articleCounter);
 
         String categoriesFromWiki = client.getCategory(category);
@@ -26,17 +31,20 @@ public class WikiPrinter {
         List<Category> categories = parseCategories(categoriesFromWiki);
 
         for (Category cat : categories) {
-            if (articleCounter >= 400) {
+            if (articleCounter >= 100) {
                 break;
             }
             if (cat.getNs().equals("0") && cat.getType().equals("page")) {
-                String articleFromWiki = client.getArticle(cat.getPageId());
-                Article article = Parser.parseArticle(articleFromWiki, cat.getPageId());
-                System.out.println(article);
+//                String articleFromWiki = client.getArticle(cat.getPageId());
+//                Article article = Parser.parseArticle(articleFromWiki, cat.getPageId());
+//                System.out.println(article);
+                File file1 = new File(fileName + File.separator + cat.getTitle() + ".txt");
+                file1.createNewFile();
                 articleCounter++;
                 System.out.println("I print article - " + articleCounter);
             } else if (cat.getType().equals("subcat") && cat.getTitle().startsWith("Категория")) {
-                print(cat.getTitle().substring("Категория".length()+1));
+                String nextCat = cat.getTitle().substring("Категория".length()+1);
+                print(nextCat, 1, fileName);
             }
         }
     }
